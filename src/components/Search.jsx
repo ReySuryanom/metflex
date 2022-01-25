55c7a0abcfdf449242ce0100e656688039922bbc
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2022-01-25 19:14:37 +0700

    create search form input

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import { BiSearch } from 'react-icons/bi';
+
+function Search() {
+  return (
+    <div className="relative w-2/3 mx-auto">
+      <BiSearch className="absolute text-xl text-white left-3 bottom-1.5" />
+      <input
+        className="w-full px-10 py-2.5 text-xs bg-transparent placeholder:text-white bg-[#929395] text-white text-center rounded-full outline-none"
+        type="text"
+        placeholder="Find movies, TV shows and more"
+      />
+    </div>
+  );
+}
+
+export default Search;
